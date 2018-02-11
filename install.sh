5e6ea5d0642030a532d502ee139a3bfd77648625
jcook793 <jcook@dotloop.com>
2018-02-10 19:28:20 -0800

    added vice settings

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.dir_colors" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.vice" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
+
 ln -sfv "$DOTFILES_DIR/Brewfile" ~
 
 echo "Done."
