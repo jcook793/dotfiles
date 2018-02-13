6c3909d331d9b76b9b8c543b541bea975323da0f
jcook793 <jcook@dotloop.com>
2018-02-13 15:09:32 -0800

    some refactoring, added some macos prefs that bug me most

--- install.sh
+++ install.sh
@@ -2,13 +2,68 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sfv "$DOTFILES_DIR/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/.dir_colors" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.vice" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
 
-ln -sfv "$DOTFILES_DIR/Brewfile" ~
+#
+# Symlinks a list of files from the dotfiles directory to ~
+#
+writeSymlinks() {
+	for filename in "$@"; do
+		echo -n "  "
+		ln -sfv "${DOTFILES_DIR}/${filename}" ~
+	done
+}
 
-echo "Done."
+#
+# Sets an NSGlobalDomain value with some feedback
+#   $1 = the key you are setting
+#   $2 = the type (int, bool, etc.)
+#   $3 = the value (you can provide more values for arrays)
+#
+setGlobalDefault() {
+	key=${1}
+	keyType=${2}
+	value=${@:3}
 
+	if [ "${keyType}" == "array" ]; then  # I am punting on arrays
+		echo "  Setting ${key} to ${value}"
+		defaults write NSGlobalDomain ${key} -${keyType} ${value}
+	else
+		currvalue=$(defaults read NSGlobalDomain ${1})
+
+		if [ "${currvalue}" == "${value}" ]; then
+			echo "  ${key} is already ${currvalue}, not changing"
+		else
+			echo "  Changing ${key} from ${currvalue} to ${value}"
+			defaults write NSGlobalDomain ${key} -${keyType} ${value}
+		fi
+	fi
+}
+
+
+#
+# Symlinks from home to here
+#
+echo "Writing symlinks"
+writeSymlinks .bash_profile .dir_colors .gitconfig .vice .vimrc Brewfile
+
+
+#
+# Some preferences
+#
+echo -e "\nSetting macOS NSGlobalDomain values"
+setGlobalDefault AppleEnableSwipeNavigateWithScrolls int 0
+setGlobalDefault AppleInterfaceStyle string Dark
+setGlobalDefault InitialKeyRepeat int 25
+setGlobalDefault KeyRepeat int 2
+setGlobalDefault NSAutomaticCapitalizationEnabled int 0
+setGlobalDefault NSAutomaticDashSubstitutionEnabled int 0
+setGlobalDefault NSAutomaticPeriodSubstitutionEnabled int 0
+setGlobalDefault NSAutomaticQuoteSubstitutionEnabled int 0
+setGlobalDefault NSAutomaticSpellingCorrectionEnabled int 0
+setGlobalDefault NSAutomaticTextCompletionEnabled int 0
+setGlobalDefault WebAutomaticSpellingCorrectionEnabled int 0
+setGlobalDefault NSUserQuotesArray array '"\""' '"\""' '"'\''"' '"'\''"'
+setGlobalDefault com.apple.mouse.scaling float 1.5
+setGlobalDefault com.apple.trackpad.forceClick int 0
+
+echo -e "\nDone."
