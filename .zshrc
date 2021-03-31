5b23a45bee0104a837f0efbe29149c9365a41b0c
John Cook <jcook@zillowgroup.com>
2021-03-31 18:44:54 -0400

    Moved to zsh, cleaned up a lot of old stuff

---
+++ .zshrc
@@ -0,0 +1,43 @@
+# Homebrew bin & sbin
+export PATH="/usr/local/sbin:/usr/local/bin:$PATH"
+
+# Homebrew Python
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# Homebrew - prefer GNU utils over Apple's
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# Vice for retro development
+export PATH="/Applications/Vice/bin:$PATH"
+
+# Any other crap I might accumulate
+export PATH="/Users/jcook/bin:$PATH"
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/jcook/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="cobalt2"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git docker docker-compose zsh-dircolors-solarized)
+
+source $ZSH/oh-my-zsh.sh
+
+# Aliases
+alias ll="ls -Alshp --group-directories-first --color"
+
+# Show a nice archey screen - use "--ofline" if you're stuck with some OpenDNS BS
+archey
--- install.sh
+++ install.sh
@@ -4,91 +4,50 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 #
-# Symlinks a list of files from the dotfiles directory to $HOME
+# Symlink utility function
 #
-writeSymlinks() {
-	for filename in "$@"; do
-		echo -n "  "
-		ln -sfv "${DOTFILES_DIR}/${filename}" "${HOME}"
-	done
-}
-
-#
-# Sets an NSGlobalDomain value with some feedback
-#   $1 = the key you are setting
-#   $2 = the type (int, bool, etc.)
-#   $3 = the value (you can provide more values for arrays)
-#
-setGlobalDefault() {
-	key=${1}
-	keyType=${2}
-	value=${@:3}
-
-	if [ "${keyType}" == "array" ]; then  # I am punting on arrays
-		echo "  Setting ${key} to ${value}"
-		defaults write NSGlobalDomain ${key} -${keyType} ${value}
-	else
-		currvalue=$(defaults read NSGlobalDomain ${1})
-
-		if [ "${currvalue}" == "${value}" ]; then
-			echo "  ${key} is already ${currvalue}, not changing"
-		else
-			echo "  Changing ${key} from ${currvalue} to ${value}"
-			defaults write NSGlobalDomain ${key} -${keyType} ${value}
-		fi
-	fi
+symlink() {
+    echo -n "  "
+    ln -sfv "${DOTFILES_DIR}/${1}" "${2}"
 }
 
 
 #
-# Symlinks
+# Simple symlinks
 #
 echo "Writing symlinks"
-writeSymlinks .bash_profile .dir_colors .gitignore_global .vimrc Brewfile
+symlink .zshrc "${HOME}"
+symlink .gitignore_global "${HOME}"
+symlink .vimrc "${HOME}"
+symlink Brewfile "${HOME}"
 
-if [ -d "${HOME}/Library/Application Support/Sublime Text 3/Packages/User" ]; then
-    echo "Found Sublime Text 3 user folder, not overwriting"
+#
+# Sublime Text configs
+#
+SUBLIME_TEXT_PACKAGES_FOLDER="${HOME}/Library/Application Support/Sublime Text 3/Packages"
+if [ -d "${SUBLIME_TEXT_PACKAGES_FOLDER}/User" ]; then
+    echo "  Found existing Sublime Text 3 user folder in ${SUBLIME_TEXT_PACKAGES_FOLDER}"
+    echo "    ^-- this might be a problem, you probably want to remove it and run this script again"
 else
-    mkdir -p "${HOME}/Library/Application Support/Sublime Text 3/Packages"
-    ln -sfv "$(pwd)/sublime/" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
+    mkdir -p "${SUBLIME_TEXT_PACKAGES_FOLDER}"
+    symlink sublime "${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
 fi
 
-
 #
-# Some OS preferences
+# Oh My Zsh theme (cobalt2)
 #
-echo -e "\nSetting macOS NSGlobalDomain values"
-setGlobalDefault AppleEnableSwipeNavigateWithScrolls int 0
-setGlobalDefault AppleInterfaceStyle string Dark
-setGlobalDefault InitialKeyRepeat int 25
-setGlobalDefault KeyRepeat int 2
-setGlobalDefault NSAutomaticCapitalizationEnabled int 0
-setGlobalDefault NSAutomaticDashSubstitutionEnabled int 0
-setGlobalDefault NSAutomaticPeriodSubstitutionEnabled int 0
-setGlobalDefault NSAutomaticQuoteSubstitutionEnabled int 0
-setGlobalDefault NSAutomaticSpellingCorrectionEnabled int 0
-setGlobalDefault NSAutomaticTextCompletionEnabled int 0
-setGlobalDefault WebAutomaticSpellingCorrectionEnabled int 0
-setGlobalDefault NSUserQuotesArray array '"\""' '"\""' '"'\''"' '"'\''"'
-setGlobalDefault com.apple.mouse.scaling float 1.5
-setGlobalDefault com.apple.trackpad.forceClick int 0
-
-#
-# Power management stuff
-#
-# Don't sleep when hooked to a charger
-sudo pmset -c sleep 0
-
-#
-# ssh
-#
-mkdir -p "${HOME}/.ssh"
-chmod 700 "${HOME}/.ssh"
+OH_MY_ZSH_THEMES_FOLDER="${HOME}/.oh-my-zsh/themes"
+if [ -d "${OH_MY_ZSH_THEMES_FOLDER}" ]; then
+    symlink oh-my-zsh/themes/cobalt2.zsh-theme "${OH_MY_ZSH_THEMES_FOLDER}"
+else
+    echo "  Could not find ${OH_MY_ZSH_THEMES_FOLDER} - is Oh My Zsh installed?"
+    echo "    ^-- this might be a problem, you probably want to install Oh My Zsh and run this script again"
+fi
 
 #
 # Global .gitignore
 #
-echo -e "\nCreating global .gitignore"
+echo -e "\nUpdating git config for global .gitignore"
 git config --global core.excludesfile "${HOME}/.gitignore_global"
 
 #
@@ -96,13 +55,10 @@ git config --global core.excludesfile "${HOME}/.gitignore_global"
 #
 echo -e "\nChecking for Homebrew"
 if which -s brew; then
-  echo "Homebrew already installed"
-else
-  echo "Installing Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  source "${HOME}/.bash_profile"
-  echo -e "\nInstalling packages from Brewfile"
+  echo "Homebrew installed, installing packages from Brewfile"
   brew bundle
+else
+  echo "Homebrew not installed yet! Go install Homebrew and run this script again."
 fi
 
 echo -e "\nDone."
